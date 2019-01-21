f9688b6010aef1010cf0ca8cc9ebb7d736c923fa
Vatsal Hirpara <34810279+vatsal-hirpara@users.noreply.github.com>
2019-01-21 16:21:25 +0530

    Create sim1.java

---
+++ sim1.java
@@ -0,0 +1,136 @@
+import java.io.*; 
+import java.util.*;
+/*
+	Vatsal Hirpara 201601239
+*/
+class Process{
+	int pid,arrival_time,burst_time;
+	public Process(int p,int a,int b){
+		pid=p;
+		arrival_time=a;
+		burst_time=b;
+	}
+}
+
+
+
+public class sim1 implements Runnable{
+
+	
+	String[] args;
+	public sim1(String[] arg) {
+    	 args=arg;
+   	}		
+		
+	public void run(){  
+	
+		String input_file = args[0];
+		String scheduling_algo=args[1];
+
+		if(scheduling_algo.equals("RR")){		
+			int time_quantum = Integer.parseInt(args[2]);
+        } 
+		
+		Scanner in=null;
+		try{
+			in = new Scanner(new File(input_file));		
+		}
+		catch(FileNotFoundException ex) {
+			System.out.println(ex);
+			System.exit(0);
+		}
+		
+		ArrayList<Process> process_list=new ArrayList<Process>(); 
+		
+		Process temp;
+		while(in.hasNextInt()){
+			temp=new Process(in.nextInt(),in.nextInt(),in.nextInt());
+			process_list.add(temp);
+		}
+		
+		int sys_time=0;
+		
+		Queue<Process> ready_queue = new LinkedList<>();
+		Process p;
+		if(scheduling_algo.equals("FCFS")){
+			
+			while(process_list.isEmpty()==false){
+				if(sys_time==p.arrival_time)
+			
+				
+				//process_list.remove(0);
+			}		
+		}
+		
+	
+		
+
+	}
+		
+	
+    public static void main(String[] args) throws FileNotFoundException { 		
+		
+		sim1 m1=new sim1(args);  
+		Thread t1 =new Thread(m1);  
+		t1.start();		
+		try{  
+  			t1.join();  
+ 		}
+ 		catch(Exception e){
+			System.out.println(e);
+		}  
+		
+	}    
+} 
+
+/*		int pid[]=new int[21];           old input
+		int arrival_time[]=new int[21];
+		int burst_time[]=new int[21];
+		
+		int i=1;
+		while(in.hasNextInt()){
+			pid[i]=in.nextInt();
+			arrival_time[i]=in.nextInt();
+			burst_time[i]=in.nextInt();
+			i++;
+		}
+*/
+
+
+
+/*		System.out.println("start");
+		try 
+		{
+    		Thread.sleep(3000);
+		}	 
+		catch(InterruptedException e)
+		{}
+		System.out.println("slept for 3");
+*/
+
+
+/*		
+		if(scheduling_algo.equals("FCFS")){
+			
+			for(Process p:process_list){
+				while(p.burst_time>0){
+					System.out.printf("<system time %d> process %d is running\n",Sys_time++,p.pid);
+					p.burst_time--;
+					
+					try 
+					{
+    					Thread.sleep(1); // CPU burst time assumed 1ms 
+					}	 
+					catch(InterruptedException e)
+					{}
+					
+				}
+				System.out.printf("<system time %d> process %d is finished.......\n",Sys_time,p.pid);
+			}
+			System.out.printf("<system time %d> All processes finish ....................",Sys_time);
+					
+		}
+*/	
+
+
+
