0f419e743236aed242f15014846dcda01894e289
Vatsal Hirpara <vatsalhirpara@yahoo.com>
2019-02-06 14:18:27 +0530

    initial commit

---
+++ vmsim.java
@@ -0,0 +1,12 @@
+/*
+	CPU Scheduling Simulator
+	Author: Vatsal Hirpara
+*/
+import java.io.*;
+import java.util.*;
+
+public class vmsim {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
